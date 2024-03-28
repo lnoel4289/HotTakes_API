7c7496fa958dd87a6d0100297eab0fdbb3d2ad1e
Laurent NOËL <lnoel4289@gmail.com>
2024-03-28 17:22:55 +0100

    envoi sur le cloud ok

--- middleware/multer-config.js
+++ middleware/multer-config.js
@@ -1,19 +1,35 @@
 const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-const MIME_TYPES = {
-  "image/jpg": "jpg",
-  "image/jpeg": "jpg",
-  "image/png": "png",
-};
+// const MIME_TYPES = {
+//   "image/jpg": "jpg",
+//   "image/jpeg": "jpg",
+//   "image/png": "png",
+// };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, "images");
-  },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(" ").join("_");
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + "." + extension);
+// const storage = multer.diskStorage({
+//   destination: (req, file, callback) => {
+//     callback(null, "images");
+//   },
+//   filename: (req, file, callback) => {
+//     const name = file.originalname.split(" ").join("_");
+//     const extension = MIME_TYPES[file.mimetype];
+//     callback(null, name + Date.now() + "." + extension);
+//   },
+// });
+
+cloudinary.config({
+  cloud_name: "db4izxstp",
+  api_key: "916193574981994",
+  api_secret: "d3yHm3qb6wdGgVyJYwVyj-MqNOI",
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "images",
+    allowed_formats: ["jpg", "jpeg", "png"],
   },
 });
 
